0ceedd0c25d6a3e146ff314a65b22604cd5a6d9c
Md. Rayhan Shorker <raihanshorker56@gmail.com>
2024-12-11 21:10:33 +0600

    change api endpoints

--- src/app/confiq/modules/car/car.route.ts
+++ src/app/confiq/modules/car/car.route.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import { CarController } from './car.controller';
 const router = express.Router();
-router.post('/create-car',CarController.createCar)
+router.post('/',CarController.createCar)
 router.get('/',CarController.getAllCar)
 router.get('/:carId',CarController.getSingleCar)
 router.put('/:carId',CarController.updateCar)
